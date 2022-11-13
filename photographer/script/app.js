db67d11154048aba476849e6abc0e612360ef5c4
Jason Kando <kando.jason@live.fr>
2022-11-13 12:55:30 +0100

    Galerie media Ok Responsive NOk

--- photographer/script/app.js
+++ photographer/script/app.js
@@ -1,21 +1,25 @@
 import { getData } from "../../utils/API.js";
-import { creatCard } from "../../utils/utils.js";
+import { createCard } from "../../utils/utils.js";
 
 // crée une Api pour recuperer les données à part! OK !
 //Utiliser le locale storage
 // TODO:faire un type module pour les fonctions voir explication
 // favoriser l'utilisation des const
-// 
+//function trie détacher la node liste avec removechild trié avec sort() et ratacher la liste avec ?  
+
 
 const getUrl = (data) => {
 const url = new URL(location.href); // transforme l'url en un objet pratique | *ajoute des méthodes à l'URL*
 const urlId = url.searchParams.get('id') // get params "id" in url
 const IDPHOTOGRAPHER = data.photographers.find((el) => {return el.id == urlId}); // search in *data.photographer* el.photographerId == urlId
-const IDMEDIA = data.media.find((el) => {return el.photographerId == urlId}); // search in *data.media* el.media == urlId
-bannerPhotographer(IDPHOTOGRAPHER, IDMEDIA)
+const IDMEDIA = data.media.filter((el) => {return el.photographerId == IDPHOTOGRAPHER.id}); // search in *data.media* el.photographerId == IDPHOTOGRAPHER.id
+bannerPhotographer(IDPHOTOGRAPHER)
+
+IDMEDIA.map((media) => {galeriePhotographer(media, IDPHOTOGRAPHER)})  
 
 }
-const bannerPhotographer = (photographer, media) => {
+
+const bannerPhotographer = (photographer) => {
 	const banner = document.querySelector('.containerPhotographer')
 	banner.innerHTML =  
 	`
@@ -27,30 +31,57 @@ const bannerPhotographer = (photographer, media) => {
 			</div>
 			<button>contacter-moi</button>
 			<div class="bannerPhotographer__image">
-				<img src="/Sample Photos/Photographers ID Photos/${photographer.portrait}" alt="" srcset="">
+				<img src="/Sample Photos/Photographers ID Photos/${photographer.portrait}" alt="${photographer.name}">
 			</div>
 			
 		</div>
 	
-	`
-	// creatCard("div", "bannerPhotographer", "containerPhotographer")
-	// creatCard("div", "bannerPhotographer__info", "bannerPhotographer")
-	// creatCard("h2", "bannerPhotographer__name", "bannerPhotographer__info").innerHTML = `${photographer.name}`
-	// creatCard("h2", "bannerPhotographer__streetPhotographe", "bannerPhotographer__info").innerHTML = `${photographer.city},${photographer.country}`
-	// creatCard("p", "bannerPhotographer__taglinePhotographe", "bannerPhotographer__info").innerHTML = `${photographer.tagline}`
-	
-	// creatCard("button", "bannerPhotographer__streetPhotographe", "bannerPhotographer__image").innerText = "contacter-moi"
-	// creatCard("div", "bannerPhotographer__image", "bannerPhotographer")
-	
-	// const imageBanner = document.createElement('img')
-	// imageBanner.src = `<img src="/Sample Photos/Photographers ID Photos/${photographer.portrait}" alt="">`	
-
+	`	
 }
 
+/**
+ * 
+ * @param {object} media 
+ * @param {object} photographer 
+ * @returns div card
+ */
+
+const galeriePhotographer = (media, photographer) => {
+	const galeriePhotographer = document.querySelector('.galeriePhotographer')
+	
+	let objetMedia = ""
+	let nomde = photographer.name.split(' ').shift() // renvoi un tableau de string séparer par une virgule et suprime le première el du tableau
+	nomde = nomde.split('-').join(' ') // renvoi un tablean de string sans le *-* entre les string et renvoi une chaine de carractère
+
+	if (media.hasOwnProperty('image')) {
+		objetMedia = `<img src="/Sample Photos/${nomde}/${media.image}" alt="">`;
+	} else if (media.hasOwnProperty('video')) {
+		objetMedia = `<video src="/Sample Photos/${nomde}/${media.video}" controls></video> alt=""`
+	}
+
+	let card = document.createElement("div")
+	card.classList.add(('card'))
+	
+	card.innerHTML = 
+	`
+		<div class="card__imageCard">
+			${objetMedia}
+		</div>
+
+		<div class="card__titre">
+			<h3>${media.title}</h3>
+		</div>
+
+		<div class="card__info">
+			<p>${media.likes}</p>
+			<span>&#x2766</span>
+		</div>
+	`
+	galeriePhotographer.appendChild(card)
+}
 
 function main(data) {
 	getUrl(data)
-	
 }
 
 window.onload = () => {
